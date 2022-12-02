f298b674a2f5d4d669b0eef38c8ae8b027436301
yassa <yassarashad3@gmail.com>
2022-12-03 01:52:46 +0200

    first commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class GameController : MonoBehaviour {
+
+	//timer
+	public Text time_Text;
+	int timer = 60;
+	//end timer
+
+	// High Score
+	int current_score = 0 ;
+	//end High Score
+
+	void Start(){
+		StartCoroutine (Timer ());
+	}
+
+	void Update () {
+		
+		updateHighScore (current_score);
+	
+	}
+
+	IEnumerator Timer(){
+
+		while(timer!=0){
+			if (timer< 10)
+				time_Text.text = "0"+timer;
+			else
+				time_Text.text =timer.ToString();
+			yield return new WaitForSeconds (1);
+			timer--;
+		}
+	}
+
+	void updateHighScore(int current_score){
+		if(current_score > PlayerPrefs.GetInt("HighScore",0))
+			PlayerPrefs.SetInt("HighScore",current_score);
+	}
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public Text high_score;
+	void Update(){
+
+	}
+
+	public void PlayGame(){
+		Time.timeScale = 1;
+	}
+		
+	public	void pause(){
+		Time.timeScale = 0;
+	}
+
+	public	void resume(){
+		Time.timeScale = 1;
+		SceneManager.LoadScene ("Level1");
+	}
+
+	public void getHighScore(){
+		high_score.text = PlayerPrefs.GetInt ("HighScore", 0).ToString();
+		Debug.Log ("High Score is "+PlayerPrefs.GetInt ("HighScore", 0).ToString());
+	}
+    //-----------------------------------------
+    //code of volume slider 
+    [SerializeField] Slider VolumeSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+
+        }
+
+
+    }
+
+    // Update is called once per frame
+    public void ChangeVolume()
+    {
+        AudioListener.volume = VolumeSlider.value;
+        Save();
+
+    }
+    void Load()
+    {
+        VolumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+
+
+    }
+    void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", VolumeSlider.value);
+    }
+    //----------------------------------------------
+}
